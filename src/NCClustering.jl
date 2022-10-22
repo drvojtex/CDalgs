d80adbca474ad64bf65a10ffd0821dc343f82df6
kozvojtex <vojtech.kozel.cz@gmail.com>
2022-10-22 18:43:56 +0200

    arbitrary ranking trashold

--- src/NCClustering.jl
+++ src/NCClustering.jl
@@ -16,7 +16,7 @@ community::Vector - current community to be updated.
 priorities::Vector - vertices sorted by the priority by which the vertices should be added.
 """
 function nc_update_community!(g::SimpleWeightedGraph{S, T}, ranking::Function,
-        community::Vector{Int64}, priorities::Vector{Int64}) where {S<:Integer, T<:Real}
+        community::Vector{Int64}, priorities::Vector{Int64}; α::Float64=0.95) where {S<:Integer, T<:Real}
 
     # vector of neighbors of the community without any community assigment
     N::Vector{Int64} = intersect(
@@ -30,11 +30,11 @@ function nc_update_community!(g::SimpleWeightedGraph{S, T}, ranking::Function,
     # add the vertex which maximise the ranking function (which result is at least 0.9)
     tmp_communities::Vector{Vector{Int64}} = map(x -> union(community, x), N) 
     new_community::Vector{Int64} = tmp_communities[argmax(map(x -> ranking(x), tmp_communities))]
-    return 0.9 <= ranking(new_community) ? new_community : community
+    return α <= ranking(new_community) ? new_community : community
 end
 
 """
-    nc_community(g, v, ranking, priorities)
+    nc_community(g, v, ranking, priorities; α)
 
 While there can be updated community starting from given vertex v::Int64, expand.
 
@@ -42,20 +42,21 @@ g::SimpleWeightedGraph - given graph.
 v::Int64 - starting vertex.
 ranking::Function - function to get ranking of the current community.
 priorities::Vector - vertices sorted by the priority by which the vertices should be added.
+α::Float64 - minimal ranking score of the community.
 """
 function nc_community(g::SimpleWeightedGraph{S, T}, v::Int64, 
-        ranking::Function, priorities::Vector{Int64}) where {S<:Integer, T<:Real}
+        ranking::Function, priorities::Vector{Int64}; α::Float64=0.95) where {S<:Integer, T<:Real}
 
     community = Vector{Int64}([v])
     while true
         l::Int64 = length(community)
-        community = nc_update_community!(g, ranking, community, priorities)
+        community = nc_update_community!(g, ranking, community, priorities, α=α)
         if l == length(community) return community end
     end
 end
 
 """
-    nc_communities(g; ranking=x->r(x))
+    nc_clustering(g; ranking=x->r(x))
 
 Find the near-clique-communities in the given graph g::SimpleWeightedGraph{Int64, Float64} by the 
 ranking function (default graph density) and priority sorting (default degree of vertex).
@@ -66,7 +67,7 @@ Default ranking is given as:
 ```
 """
 function nc_clustering(g::SimpleWeightedGraph{S, T}; 
-        ranking=Nothing, priority=Nothing) where {S<:Integer, T<:Real}
+        ranking=Nothing, priority=Nothing, α::Float64=0.95) where {S<:Integer, T<:Real}
 
     if ranking == Nothing
         ranking = x::Vector{Int64} -> length(x) == 1 ? 1 : Graphs.density(g[x])
@@ -85,7 +86,7 @@ function nc_clustering(g::SimpleWeightedGraph{S, T};
         sort!(priorities, by=priority, rev=true)
 
         # find new community
-        new_com::Set{Int64} = Set(nc_community(g, priorities[1], ranking, priorities))
+        new_com::Set{Int64} = Set(nc_community(g, priorities[1], ranking, priorities, α=α))
         push!(communities, new_com)
         union!(used_vertices, new_com)
 
@@ -100,4 +101,4 @@ function nc_clustering(g::SimpleWeightedGraph{S, T};
     return Int.(result)
 end
 
-nc_clustering(g::SimpleGraph{Int64}) = nc_communities(SimpleWeightedGraph(g))
+nc_clustering(g::SimpleGraph{Int64}) = nc_clustering(SimpleWeightedGraph(g))
